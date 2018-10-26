e79b1c5249494f171aa08647bd938bba768d6082
Mateusz Hackiewicz <M.Hackiewicz@tt.com.pl>
2018-10-26 10:42:44 +0100

    Updates

---
+++ precache-manifest.abe51d25ccfcd68e4c103651f809f752.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e0e33769c5bf8245699",
+    "url": "/performance-testing-react-app/static/js/runtime~main.4e0e3376.js"
+  },
+  {
+    "revision": "1a32efe44bb4ae675e0c",
+    "url": "/performance-testing-react-app/static/js/main.1a32efe4.chunk.js"
+  },
+  {
+    "revision": "e024e23f9e6dafcd9ae9",
+    "url": "/performance-testing-react-app/static/js/1.e024e23f.chunk.js"
+  },
+  {
+    "revision": "1a32efe44bb4ae675e0c",
+    "url": "/performance-testing-react-app/static/css/main.40bb0831.chunk.css"
+  },
+  {
+    "revision": "e024e23f9e6dafcd9ae9",
+    "url": "/performance-testing-react-app/static/css/1.9fb286e6.chunk.css"
+  },
+  {
+    "revision": "b890f39c2128a1d622205bf56ad63f2e",
+    "url": "/performance-testing-react-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pwa-app/precache-manifest.eab4c62cc1ad340ecdbdf3454de6ae99.js"
+  "/performance-testing-react-app/precache-manifest.abe51d25ccfcd68e4c103651f809f752.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/pwa-app/index.html", {
+workbox.routing.registerNavigationRoute("/performance-testing-react-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
