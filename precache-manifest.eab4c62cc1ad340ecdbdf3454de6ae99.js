8be42ab2f72eca1e88a3c3c7eff1c325c6ad480d
Mateusz Hackiewicz <M.Hackiewicz@tt.com.pl>
2018-10-26 10:38:28 +0100

    Updates

---
+++ precache-manifest.eab4c62cc1ad340ecdbdf3454de6ae99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "89bf7b874f2df08752b4",
+    "url": "/pwa-app/static/js/runtime~main.89bf7b87.js"
+  },
+  {
+    "revision": "61769acfce80e5493073",
+    "url": "/pwa-app/static/js/main.61769acf.chunk.js"
+  },
+  {
+    "revision": "e024e23f9e6dafcd9ae9",
+    "url": "/pwa-app/static/js/1.e024e23f.chunk.js"
+  },
+  {
+    "revision": "61769acfce80e5493073",
+    "url": "/pwa-app/static/css/main.40bb0831.chunk.css"
+  },
+  {
+    "revision": "e024e23f9e6dafcd9ae9",
+    "url": "/pwa-app/static/css/1.9fb286e6.chunk.css"
+  },
+  {
+    "revision": "c6f4b6ac7c093db56cd6c1b7a7d49236",
+    "url": "/pwa-app/index.html"
+  }
+];
